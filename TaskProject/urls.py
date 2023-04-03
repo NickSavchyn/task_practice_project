06e60f576bbeb2260534403de0d9cb340f0a83e9
Mykola Savchyn <kola.savchyn@gmail.com>
2023-04-03 11:56:53 +0300

    Add base.html Add task list view

--- TaskProject/urls.py
+++ TaskProject/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from TaskProject.views import TaskListView
+
 urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
     ]
 
 app_name = "TaskProject"
--- TaskProject/views.py
+++ TaskProject/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic, View
 
-# Create your views here.
+from TaskProject.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "TaskProject/task_list.html"
+    paginate_by = 5
+    queryset = Task.objects.prefetch_related("tags")
+

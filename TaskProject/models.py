ce174bcdba2dc06da3bf2ae0fe5ad6a305c79956
Mykola Savchyn <kola.savchyn@gmail.com>
2023-04-03 11:10:58 +0300

    Add migrations

--- TaskProject/models.py
+++ TaskProject/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(to=Tag, related_name="tags")
+
+    def __str__(self):
+        return f"({self.content} ({self.tags}))"
+
